2a8428bde71e1e313c9c70ec3064c8f3619e9dce
Kevin Renskers <kevin@loopwerk.io>
2022-09-07 11:44:13 +0200

    Fix hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,6 @@
 import type { Handle } from "@sveltejs/kit";
-import cookie from "cookie";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const cookies = cookie.parse(event.cookies.get("cookie") || "");
-  event.locals.token = cookies.token;
+  event.locals.token = event.cookies.get("token") || "";
   return await resolve(event);
 };
