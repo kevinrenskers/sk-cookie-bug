4009b4919e3e7c7615d383f228ecd70ff91899b3
Kevin Renskers <kevin@loopwerk.io>
2022-09-07 11:56:19 +0200

    Simplify further

--- src/routes/login/[token]/+page.svelte
+++ src/routes/login/[token]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import { goto, invalidateAll } from "$app/navigation";
-  import { postLocal } from "$lib/api";
 
   async function login() {
     const payload = {
@@ -9,13 +8,9 @@
     };
 
     try {
-      const res = await postLocal<{ token: string }>(
-        fetch,
-        "/auth/exchange-magic-token",
-        payload
-      );
-
-      const token = res.token;
+      const res = await fetch("/auth/exchange-magic-token", { method: "POST" });
+      const json = await res.json();
+      const token = json.token;
       if (token) {
         console.log(token);
         await invalidateAll();
