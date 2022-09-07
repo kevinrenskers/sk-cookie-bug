b9d378f339e31ef17f9c5b92c9c323d7a403bd58
Kevin Renskers <kevin@loopwerk.io>
2022-09-07 11:49:03 +0200

    Add a comment

--- src/routes/auth/exchange-magic-token/+server.ts
+++ src/routes/auth/exchange-magic-token/+server.ts
@@ -1,6 +1,7 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ cookies }) => {
+  // In the real world app I'd call an external endpoint to fetch the token
   const token = "ABC123";
   console.log(`Storing token in cookie: ${token}`);
   cookies.set("token", token, { path: "/", httpOnly: true, maxAge: 31536000 });
